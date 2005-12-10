8fc847f8a7fb6e57a2e1b062a0a7a78b9e1939e2
Max Kremmel <xing@synapse.plus.com>
2005-12-10 10:53:26 +0000

    use PKG_DIR as key for registerAppMenu

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author   xing <xing@synapse.plus.com>
- * @version  $Revision: 1.1 $
+ * @version  $Revision: 1.2 $
  * @package  Pigeonholes
  * @subpackage functions
  */
@@ -10,7 +10,7 @@ $gBitSystem->registerPackage( 'chatterbox', dirname( __FILE__).'/' );
 
 if( $gBitSystem->isPackageActive( 'chatterbox' ) ) {
 	if( $gBitUser->hasPermission( 'bit_p_use_chatterbox' ) ) {
-		$gBitSystem->registerAppMenu( 'chatterbox', 'Chatterbox', CHATTERBOX_PKG_URL.'index.php', 'bitpackage:chatterbox/menu_chatterbox.tpl', 'Chatterbox' );
+		$gBitSystem->registerAppMenu( CHATTERBOX_PKG_DIR, 'Chatterbox', CHATTERBOX_PKG_URL.'index.php', 'bitpackage:chatterbox/menu_chatterbox.tpl', CHATTERBOX_PKG_NAME );
 	}
 }
 ?>
