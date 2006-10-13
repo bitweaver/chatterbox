2251d40be70cc7a37028d3e5029a0f2297ac3f51
Lester Caine <lester@lsces.co.uk>
2006-10-13 12:43:12 +0000

    phpDoc additions

--- get_id.php
+++ get_id.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_chatterbox/get_id.php,v 1.3 2006/10/13 12:43:12 lsces Exp $
+ *
+ * +----------------------------------------------------------------------+
+ * | Copyright ( c ) 2004, bitweaver.org
+ * +----------------------------------------------------------------------+
+ * | All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * | Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ * |
+ * | For comments, please use phpdocu.sourceforge.net documentation standards!!!
+ * | -> see http://phpdocu.sourceforge.net/
+ * +----------------------------------------------------------------------+
+ * | Authors: xing <xing@synapse.plus.com>
+ * +----------------------------------------------------------------------+
+ *
+ * Chatterbox class
+ *
+ * @author   xing <xing@synapse.plus.com>
+ * @version  $Revision: 1.3 $
+ * @package  chatterbox
+ */
+
+/**
+ * required setup
+ */
 require_once( '../bit_setup_inc.php' );
 echo $gBitSystem->mDb->getOne( "SELECT MAX( `chatterbox_id` ) FROM `".BIT_DB_PREFIX."chatterbox`" );
 ?>
