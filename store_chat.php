c3014ade426386178cbc7e75709a49837f101046
wjames5 <will@tekimaki.com>
2009-10-01 14:16:59 +0000

    remove ref to non-existant copyright.txt file

--- store_chat.php
+++ store_chat.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_chatterbox/store_chat.php,v 1.3 2009/10/01 13:45:33 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_chatterbox/store_chat.php,v 1.4 2009/10/01 14:16:59 wjames5 Exp $
  *
  * +----------------------------------------------------------------------+
  * | Copyright ( c ) 2004, bitweaver.org
  * +----------------------------------------------------------------------+
- * | All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * | All Rights Reserved. See below for details and a complete list of authors.
  * | Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details
  * |
  * | For comments, please use phpdocu.sourceforge.net documentation standards!!!
@@ -17,7 +17,7 @@
  * Chatterbox class
  *
  * @author   xing <xing@synapse.plus.com>
- * @version  $Revision: 1.3 $
+ * @version  $Revision: 1.4 $
  * @package  chatterbox
  */
 
